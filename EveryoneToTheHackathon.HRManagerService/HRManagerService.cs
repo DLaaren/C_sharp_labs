2c2baa18194222d573005626f45fd749671230e8
DLaaren <dlaaren8@gmail.com>
2024-09-26 15:12:36 +0700

    EmployeeServices and HrManagerService work perfectly

--- EveryoneToTheHackathon.HRManagerService/HRManagerService.cs
+++ EveryoneToTheHackathon.HRManagerService/HRManagerService.cs
@@ -1,88 +1,11 @@
-using System.Text;
-using System.Text.Json;
-using EveryoneToTheHackathon.Dtos;
 using EveryoneToTheHackathon.Entities;
 
 namespace EveryoneToTheHackathon.HRManagerService;
 
-public class HRManagerService : BackgroundService
+public class HrManagerService(HRManager hrManager)
 {
-    private readonly ILogger<HRManagerService> _logger;
-    private readonly HttpClient _httpClient;
-    private readonly HRManager _hrManager;
-    //public int EmployeesNumber { get; init; }
-    public IEnumerable<Employee>? Employees { get; set; }
-    public IEnumerable<Wishlist>? Wishlists { get; set; }
+    public HRManager HrManager { get; } = hrManager;
 
-    public HRManagerService(ILogger<HRManagerService> logger, HttpClient httpClient, HRManager hrManager)//, int employeesNumber)
-    {
-        _logger = logger;
-        _httpClient = httpClient;
-        _hrManager = hrManager;
-        //EmployeesNumber = employeesNumber;
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        _logger.LogInformation("HRManager waiting for employees;");
-
-        while (Employees == null)
-            await Task.Delay(1000, stoppingToken);
-            
-        await SendEmployeesAsync(Employees!, stoppingToken);
-        _logger.LogInformation("HRManager has sent employees;");
-        
-        
-        _logger.LogInformation("HRManager waiting for wishlists;");
-        while (Wishlists == null)
-            await Task.Delay(1000, stoppingToken);
-        
-        await SendWishlistsAsync(Wishlists!, stoppingToken);
-        _logger.LogInformation("HRManager has sent wishlists;");
-
-        
-        IEnumerable<Team> teams = _hrManager.BuildTeams(
-            Employees!.Where(e => e.Title.Equals(EmployeeTitle.TeamLead)), 
-            Employees!.Where(e => e.Title.Equals(EmployeeTitle.Junior)),
-            Wishlists!.Where(w => w.EmployeeTitle.Equals(EmployeeTitle.TeamLead)),
-            Wishlists!.Where(w => w.EmployeeTitle.Equals(EmployeeTitle.Junior)));
-        _logger.LogInformation("HRManager has created teams;");
-
-        await SendTeamsAsync(teams, stoppingToken);
-        _logger.LogInformation("HRManager has sent teams;");
-
-        await Task.CompletedTask;
-    }
-    
-    private async Task SendEmployeesAsync(IEnumerable<Employee> employees, CancellationToken stoppingToken)
-    {
-        var employeeDtos = employees.Select(e => new EmployeeDto(e.Id, e.Title, e.Name));
-        
-        var content = new StringContent(JsonSerializer.Serialize(employeeDtos), Encoding.UTF8, "application/json");
-        var response = await _httpClient.PostAsync(_httpClient.BaseAddress + "api/hr_director/employees", content, stoppingToken);
-        response.EnsureSuccessStatusCode();
-        await Task.CompletedTask;
-    }
-    
-    private async Task SendWishlistsAsync(IEnumerable<Wishlist> wishlists, CancellationToken stoppingToken)
-    {
-        var wishlistDtos = wishlists.Select(w => new WishlistDto(w.EmployeeId, w.EmployeeTitle, w.DesiredEmployees));
-        
-        var content = new StringContent(JsonSerializer.Serialize(wishlistDtos), Encoding.UTF8, "application/json");
-        var response = await _httpClient.PostAsync(_httpClient.BaseAddress + "api/hr_director/wishlists", content, stoppingToken);
-        response.EnsureSuccessStatusCode();
-        await Task.CompletedTask;
-    }
-    
-    private async Task SendTeamsAsync(IEnumerable<Team> teams, CancellationToken stoppingToken)
-    {
-        var teamDtos = teams.Select(t => new TeamDto(
-            new EmployeeDto(t.TeamLead.Id, t.TeamLead.Title, t.TeamLead.Name), 
-            new EmployeeDto(t.Junior.Id, t.Junior.Title, t.Junior.Name)));
-        
-        var content = new StringContent(JsonSerializer.Serialize(teamDtos), Encoding.UTF8, "application/json");
-        var response = await _httpClient.PostAsync(_httpClient.BaseAddress + "api/hr_director/teams", content, stoppingToken);
-        response.EnsureSuccessStatusCode();
-        await Task.CompletedTask;
-    }
+    public List<Employee>? Employees { get; set; }
+    public List<Wishlist>? Wishlists { get; set; }
 }
